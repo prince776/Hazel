3b3555628aa0a58383d03743d5c9f284b149fe0c
prince776 <princemit776@gmail.com>
2020-06-29 19:07:29 +0530

    Added events to ImGui

--- Hazel/src/Hazel/Events/KeyEvent.h
+++ Hazel/src/Hazel/Events/KeyEvent.h
@@ -56,4 +56,27 @@ namespace Hazel {
 
 		EVENT_CLASS_TYPE(KeyReleased)
 	};
+
+	class HAZEL_API KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(int keycode)
+			: KeyEvent(keycode)
+		{
+		}
+
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeats)";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped)
+	private:
+		int m_RepeatCount;
+	};
+
 }
--- Hazel/src/Hazel/ImGui/ImGuiLayer.h
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.h
@@ -2,6 +2,11 @@
 
 #include "hazel/Layer.h"
 
+#include "Hazel/Events/Event.h"
+#include "Hazel/Events/MouseEvent.h"
+#include "Hazel/Events/KeyEvent.h"
+#include "Hazel/Events/ApplicationEvent.h"
+
 namespace Hazel {
 
 	class HAZEL_API ImGuiLayer : public Layer
@@ -15,6 +20,16 @@ namespace Hazel {
 		void OnDetach() override;
 		void OnUpdate() override;
 		void OnEvent(Event& event) override;
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 
 	private:
 		float m_Time = 0.0f;
