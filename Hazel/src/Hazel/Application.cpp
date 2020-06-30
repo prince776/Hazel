6336bb86a969df1aa93b37466944f6da2a801c3c
prince776 <princemit776@gmail.com>
2020-06-30 19:24:27 +0530

    Added glm

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -7,6 +7,8 @@
 
 #include "Input.h"
 
+#include <glm/glm.hpp>
+
 namespace Hazel {
 	
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -65,8 +67,8 @@ namespace Hazel {
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
 
-			auto[x, y] = Input::GetMousePosition();
-			HZ_CORE_TRACE("{0}, {1}", x, y);
+			//auto[x, y] = Input::GetMousePosition();
+			//HZ_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
