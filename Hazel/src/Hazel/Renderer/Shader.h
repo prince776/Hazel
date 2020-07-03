2b6ea196cdf44c43d07a4fa5f6903123f1ef496e
prince776 <princemit776@gmail.com>
2020-07-03 16:10:25 +0530

    Added Basic Shader, still need to reorganize and abstract

---
+++ Hazel/src/Hazel/Renderer/Shader.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Hazel/Core.h"
+
+namespace Hazel {
+
+	class HAZEL_API Shader
+	{
+	public:
+		Shader(const std::string& vertexSrc, const std::string& fragmentSrc);
+		~Shader();
+
+		void Bind() const;
+		void Unbind() const;
+	private:
+		uint32_t m_RendererID;
+	};
+}
