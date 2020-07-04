0fa52895168135ab3794d8214960004286d99608
prince776 <princemit776@gmail.com>
2020-07-04 14:18:45 +0530

    Added OpenGL Buffer abstraction

---
+++ Hazel/src/Hazel/Renderer/Renderer.cpp
@@ -0,0 +1,9 @@
+#include "hzpch.h"
+#include "Renderer.h"
+
+namespace Hazel {
+
+	// For now manually specifying the API to be used.
+	RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+
+}
