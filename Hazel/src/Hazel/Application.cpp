bab386aacd81c17ec94bd4da8edcaba917be5266
prince776 <princemit776@gmail.com>
2020-07-04 17:53:49 +0530

    Added Vertex buffer layout descriptors

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -8,6 +8,7 @@
 #include "Input.h"
 
 #include <glm/glm.hpp>
+#include "Platform/OpenGL/OpenGLBuffer.h"
 
 namespace Hazel {
 	
@@ -37,18 +38,36 @@ namespace Hazel {
 		glBindVertexArray(m_VertexArray);
 
 		// Create Vertex Buffer
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
-		
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+			m_VertexBuffer->SetLayout(layout);
+		}
 
 		// Add Vertex Buffer to Vertex Array.
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		uint32_t index = 0;
+		const BufferLayout& layout = m_VertexBuffer->GetLayout();
+
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
 		// Create Index Buffer to specify render order for vertices.
 		uint32_t indices[3] = { 0, 1, 2 };
@@ -58,10 +77,15 @@ namespace Hazel {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
 			out vec3 v_Position;
+			out vec4 v_Color;
+
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);	
 			}
 		)";
@@ -71,9 +95,12 @@ namespace Hazel {
 			
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
+			in vec4 v_Color;
+
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
--- Hazel/src/Platform/OpenGL/OpenGLBuffer.h
+++ Hazel/src/Platform/OpenGL/OpenGLBuffer.h
@@ -1,8 +1,30 @@
 #pragma once
 
 #include "Hazel/Renderer/Buffer.h"
+#include <glad/glad.h>
 
 namespace Hazel {
+
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case Hazel::ShaderDataType::Float:    return GL_FLOAT;
+		case Hazel::ShaderDataType::Float2:   return GL_FLOAT;
+		case Hazel::ShaderDataType::Float3:   return GL_FLOAT;
+		case Hazel::ShaderDataType::Float4:   return GL_FLOAT;
+		case Hazel::ShaderDataType::Mat3:     return GL_FLOAT;
+		case Hazel::ShaderDataType::Mat4:     return GL_FLOAT;
+		case Hazel::ShaderDataType::Int:      return GL_INT;
+		case Hazel::ShaderDataType::Int2:     return GL_INT;
+		case Hazel::ShaderDataType::Int3:     return GL_INT;
+		case Hazel::ShaderDataType::Int4:     return GL_INT;
+		case Hazel::ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
 
 	class OpenGLVertexBuffer : public VertexBuffer
 	{
@@ -12,8 +34,12 @@ namespace Hazel {
 
 		virtual void Bind() const;
 		virtual void Unbind() const;
+
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
