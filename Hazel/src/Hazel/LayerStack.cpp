b406967601daf90cf6edfe72485d1aae7b1f2464
prince776 <princemit776@gmail.com>
2020-07-05 16:59:49 +0530

    Calling onDetach() before removing from layer stack now

--- Hazel/src/Hazel/LayerStack.cpp
+++ Hazel/src/Hazel/LayerStack.cpp
@@ -32,9 +32,9 @@ namespace Hazel {
 		auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
 		if (it != m_Layers.end())
 		{
+			layer->OnDetach();
 			m_Layers.erase(it);
 			m_LayerInsertIndex--;
-			layer->OnDetach();
 		}
 	}
 
@@ -43,8 +43,8 @@ namespace Hazel {
 		auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
 		if (it != m_Layers.end())
 		{
-			m_Layers.erase(it);
 			overlay->OnDetach();
+			m_Layers.erase(it);
 		}
 	}
 }
