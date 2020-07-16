d1567b88ae0d004e11b2688f2a12e2d4b9edd8b0
prince776 <princemit776@gmail.com>
2020-07-16 13:19:50 +0530

    Added basic Batch rendering(only color included)

--- Hazel/src/Hazel/Renderer/RenderCommand.h
+++ Hazel/src/Hazel/Renderer/RenderCommand.h
@@ -29,9 +29,9 @@ namespace Hazel {
 			s_RendererAPI->Clear();
 		}
 
-		inline static void DrawIndexed(const Hazel::Ref<VertexArray>& vertexArray)
+		inline static void DrawIndexed(const Hazel::Ref<VertexArray>& vertexArray, uint32_t  count = 0)
 		{
-			s_RendererAPI->DrawIndexed(vertexArray);
+			s_RendererAPI->DrawIndexed(vertexArray, count);
 		}
 	private:
 		static Scope<RendererAPI> s_RendererAPI;
